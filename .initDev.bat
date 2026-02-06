deb0a89b8a452f73a98d2089218c2979294abb1d
Dominic Moser <dominic@dmoser.com>
2026-02-06 12:32:16 +0100

    fix: readd submodule init to the init script

--- .initDev.bat
+++ .initDev.bat
@@ -3,4 +3,12 @@ setlocal enabledelayedexpansion
 
 :: Execute this file after first cloning the repository.
 
+cd /d "%~dp0"
+echo Updating git submodules...
+git submodule update --init --recursive
+if %ERRORLEVEL% neq 0 (
+    echo ❌ Failed to update submodules.
+    exit /b %ERRORLEVEL%
+)
+
 call "%~dp0toolkit\scripts\initDevelopmentEnvironment.bat" "%~dp0"
--- .initDev.sh
+++ .initDev.sh
@@ -5,4 +5,9 @@
 set -e
 
 SCRIPT_DIR=$(CDPATH= cd -- "$(dirname -- "$0")" && pwd) || exit 1
+cd "$SCRIPT_DIR"
+
+echo "Updating git submodules..."
+git submodule update --init --recursive
+
 sh "$SCRIPT_DIR/toolkit/scripts/initDevelopmentEnvironment.sh" "$SCRIPT_DIR"
